4d3b4ec6fd9c98e74d97810a08452d9ec5bbdae8
Daniel Suh <dansuh3593@gmail.com>
2018-05-12 12:11:48 -0400

    Finish color input component

--- src/components/colorInput.vue
+++ src/components/colorInput.vue
@@ -1,11 +1,25 @@
 <template>
+  <div className="color-input">
+    <Chrome :value="color" @input="handleInput"/>
+  </div>
 </template>
 
 <script>
+import { Chrome } from 'vue-color'
+
 export default {
   name: 'ColorInput',
+  components: {
+    Chrome
+  },
   props: {
-    msg: String
+    changeColor: Function,
+    color: String
+  },
+  methods: {
+    handleInput: function(e) {
+      this.changeColor(e.hex)
+    }
   }
 }
 </script>
