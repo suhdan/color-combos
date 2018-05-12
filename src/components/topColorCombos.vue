bd3ccb4009e2610239614b02ec79f211ba93fdf9
Daniel Suh <dansuh3593@gmail.com>
2018-05-12 12:11:22 -0400

    Finish top combos fetch component

--- src/components/topColorCombos.vue
+++ src/components/topColorCombos.vue
@@ -1,14 +1,30 @@
 <template>
-  <div v-on:click="changeColor('white')">
-      hi
+  <div>
+    <ul>
+      <li v-for="combo in topCombos" v-on:click="changeColor(combo.color_one)">
+        {{combo.color_one}}        
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'TopColorCombos',
   props: {
     changeColor: Function
+  },
+  data() {
+    return {
+      topCombos: []
+    }
+  },
+  created: function() {
+    axios.get('https://randoma11y.com/combos/top').then( res => {
+      this.topCombos = res.data.slice(0,10)
+    })
   }
 }
 </script>
