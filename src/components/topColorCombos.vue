4d8515492ecb745b7f7d24cf694b0b663f14add9
Daniel Suh <dansuh3593@gmail.com>
2018-05-13 14:53:52 -0400

    Ensure hexcodes are six digit

--- src/components/topColorCombos.vue
+++ src/components/topColorCombos.vue
@@ -22,9 +22,28 @@ export default {
     }
   },
   created: function() {
-    axios.get('https://randoma11y.com/combos/top').then( res => {
-      this.topCombos = res.data.slice(0,10)
-    })
+    this.fetchTopCombos()
+  },
+  methods: {
+    fetchTopCombos: function() {
+      axios.get('https://randoma11y.com/combos/top').then( res => {
+        this.topCombos = res.data.slice(0,10).map( combo => {
+          combo.color_one = this.ensureSixDigitHex(combo.color_one)
+          return combo
+        })
+      })
+    },
+    ensureSixDigitHex: function(hexCode) {
+      let hashlessCode = hexCode.slice(1)
+      if (hashlessCode.length === 6)
+        return hexCode
+
+      let sixDigitHex = "#"
+      for (var i = 0; i < hashlessCode.length; i++) {
+        sixDigitHex += hashlessCode[i] + hashlessCode[i]
+      }
+      return sixDigitHex
+    }
   }
 }
 </script>
